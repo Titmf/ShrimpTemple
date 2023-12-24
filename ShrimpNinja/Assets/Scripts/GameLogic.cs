8fe2ac1f56f35a499f680f0e07df0599d7787e97
Titmf <mima.miheev.tm@gmail.com>
2023-12-24 23:45:58 +0400

    fixed

--- ShrimpNinja/Assets/Scripts/GameLogic.cs
+++ ShrimpNinja/Assets/Scripts/GameLogic.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    [SerializeField] private GameObject _gameOver;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Time.timeScale = 0;
-        Debug.Log("Game Over");
+        _gameOver.SetActive(true);
     }
 }
